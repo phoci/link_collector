6116b2a582d47c461f8f7ac18a03d2f5b3d87600
vps <phociat@yandex.com>
2022-10-13 16:22:00 +0300

    Tackling copying to clipboard through creating contnent scripts on every tab (thanks v3).

--- background.js
+++ background.js
@@ -2,28 +2,36 @@ links = new Map();
 
 chrome.contextMenus.create({
     id: "addToLinksList",
-    title: "Add to links",
+    title: "Add to links list",
     contexts: ["selection"],
-});
+})
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     addToLinksList(info.selectionText, tab)
 }
 )
 
-chrome.commands.onCommand.addListener((command, tab) => {
-    console.log(`Command "${command}" triggered`);
-    /*
-    if (`${command}` == "add-link") {
-        console.log(`Command "aaa" triggered`);
+chrome.commands.onCommand.addListener((command) => {
+    if (command == "copy-to-clipboard") {
+        console.log(`Command "${command}" triggered`);
+        textToCopy = ""
+        for (let [k, v] of links) {
+            console.log(v + " " + k + "\n");
+            textToCopy = textToCopy + (v + " " + k + "\n");
+        }
+        //
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id,
+                {
+                    message: "copyText",
+                    textToCopy: textToCopy
+                }, function (response) { })
+        })
     }
-    */
-    addToLinksList(chrome.document.getElementById("output").value, tab)
-});
+})
 
 function addToLinksList(text, tab) {
     console.log(text + " " + tab.url + "\n");
-    //links.push({"url": tab.url, "text": info.selectionText})
     links.set(tab.url, text)
     console.log(links)
 }
